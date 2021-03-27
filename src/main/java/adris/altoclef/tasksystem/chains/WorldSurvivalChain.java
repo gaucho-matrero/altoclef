1ae6a92119252000097bfc3322125990f08c3749
Adris Jautakas <adrisj7@gmail.com>
2021-03-26 20:02:58 -0400

    General survival chain: Don't drown

---
+++ src/main/java/adris/altoclef/tasksystem/chains/WorldSurvivalChain.java
@@ -0,0 +1,65 @@
+package adris.altoclef.tasksystem.chains;
+
+import adris.altoclef.AltoClef;
+import adris.altoclef.Debug;
+import adris.altoclef.tasksystem.TaskRunner;
+import baritone.api.utils.input.Input;
+import net.minecraft.client.MinecraftClient;
+
+public class WorldSurvivalChain extends SingleTaskChain {
+
+    private boolean _wasAvoidingDrowning;
+
+    public WorldSurvivalChain(TaskRunner runner) {
+        super(runner);
+    }
+
+    @Override
+    protected void onTaskFinish(AltoClef mod) {
+
+    }
+
+    @Override
+    public float getPriority(AltoClef mod) {
+        handleDrowning(mod);
+        return Float.NEGATIVE_INFINITY;
+    }
+
+    private void handleDrowning(AltoClef mod) {
+        // Swim
+        boolean avoidedDrowning = false;
+        if (mod.getModSettings().shouldAvoidDrowning()) {
+            if (!mod.getClientBaritone().getPathingBehavior().isPathing()) {
+                if (mod.getPlayer().isTouchingWater() && mod.getPlayer().getAir() < mod.getPlayer().getMaxAir()) {
+                    // Swim up!
+                    Debug.logInternal("SWIM UP!");
+                    MinecraftClient.getInstance().options.keyJump.setPressed(true);
+                    //mod.getClientBaritone().getInputOverrideHandler().setInputForceState(Input.JUMP, true);
+                    avoidedDrowning = true;
+                    _wasAvoidingDrowning = true;
+                } else {
+                    Debug.logInternal(mod.getPlayer().isTouchingWater() + " " + mod.getPlayer().getAir() + " / " + mod.getPlayer().getMaxAir());
+                }
+            } else {
+                Debug.logInternal("not pathing");
+            }
+        }
+        // Stop swimming up if we just swam.
+        if (_wasAvoidingDrowning && !avoidedDrowning) {
+            _wasAvoidingDrowning = false;
+            MinecraftClient.getInstance().options.keyJump.setPressed(false);
+            //mod.getClientBaritone().getInputOverrideHandler().setInputForceState(Input.JUMP, false);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Misc World Survival Chain";
+    }
+
+    @Override
+    public boolean isActive() {
+        // Always check for survival.
+        return true;
+    }
+}
