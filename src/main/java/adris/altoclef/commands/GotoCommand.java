734d1d6f0e7cdc71dd3b344a669b26597e5e54c0
BlueBurgersTDD <58081413+TheBlueBurger@users.noreply.github.com>
2021-06-15 17:13:46 +0200

    Update GotoCommand.java

--- src/main/java/adris/altoclef/commands/GotoCommand.java
+++ src/main/java/adris/altoclef/commands/GotoCommand.java
@@ -13,14 +13,19 @@ public class GotoCommand extends Command {
     private static final int EMPTY = -1;
 
     public GotoCommand() throws CommandException {
-        super("goto", "Tell bot to travel to a set of coordinates.", new Arg(Integer.class, "x"), new Arg(Integer.class, "y", EMPTY, 2), new Arg(Integer.class, "z"));
+        super("goto", "Tell bot to travel to a set of coordinates.", new Arg(String.class, "dimension", String.class, 0, false), new Arg(Integer.class, "x"), new Arg(Integer.class, "y", EMPTY, 2), new Arg(Integer.class, "z"));
     }
 
     @Override
     protected void Call(AltoClef mod, ArgParser parser) throws CommandException {
+        String dimension = parser.Get(String.class);
         int x = parser.Get(Integer.class),
                 y = parser.Get(Integer.class),
                 z = parser.Get(Integer.class);
+        Object[] things = {dimension, x, y, z};
+        for(Object thing : things) {
+            System.out.println(thing);
+        }
         if (y != EMPTY) {
             mod.runUserTask(new GetToBlockTask(new BlockPos(x, y, z), false), nothing -> finish());
         } else {
