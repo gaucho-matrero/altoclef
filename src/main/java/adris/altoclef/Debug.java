b96d9587c4518b80776e9290bbaf0247b1fa22bb
Adris Jautakas <adrisj7@gmail.com>
2021-10-01 00:30:19 -0400

    Make Debug logs work even if MinecraftClient.getInstance() is null

--- src/main/java/adris/altoclef/Debug.java
+++ src/main/java/adris/altoclef/Debug.java
@@ -15,7 +15,7 @@ public class Debug {
     }
 
     public static void logMessage(String message, boolean prefix) {
-        if (MinecraftClient.getInstance().player != null) {
+        if (MinecraftClient.getInstance() != null && MinecraftClient.getInstance().player != null) {
             if (prefix) {
                 message = "\u00A72\u00A7l\u00A7o[Alto Clef] \u00A7r" + message;
             }
@@ -36,7 +36,7 @@ public class Debug {
 
     public static void logWarning(String message) {
         logInternal("WARNING: " + message);
-        if (MinecraftClient.getInstance().player != null) {
+        if (MinecraftClient.getInstance() != null && MinecraftClient.getInstance().player != null) {
             String msg = "\u00A72\u00A7l\u00A7o[Alto Clef] \u00A7c" + message + "\u00A7r";
             MinecraftClient.getInstance().player.sendMessage(Text.of(msg), false);
             //MinecraftClient.getInstance().player.sendChatMessage(msg);
@@ -52,7 +52,7 @@ public class Debug {
         System.err.println(message);
         System.err.println("at:");
         System.err.println(stacktrace);
-        if (MinecraftClient.getInstance().player != null) {
+        if (MinecraftClient.getInstance() != null && MinecraftClient.getInstance().player != null) {
             String msg = "\u00A72\u00A7l\u00A7c[Alto Clef ERROR]" + message + "\nat:\n" + stacktrace + "\u00A7r";
             MinecraftClient.getInstance().player.sendMessage(Text.of(msg), false);
         }
