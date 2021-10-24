bebd001e69f9554ece2d3a731b397cdabbcb0b97
Adris Jautakas <adrisj7@gmail.com>
2021-10-24 02:39:51 -0400

    This didn't work lol

--- src/main/java/adris/altoclef/util/helpers/StlHelper.java
+++ src/main/java/adris/altoclef/util/helpers/StlHelper.java
@@ -26,26 +26,4 @@ public interface StlHelper {
     static <T> String toString(T[] thing, Function<T, String> toStringFunc) {
         return toString(Arrays.asList(thing), toStringFunc);
     }
-
-    @SafeVarargs
-    static <T> Collection<T> combine(Collection<T>... collections) {
-        int size = Arrays.stream(collections).reduce(0, (count, collection) -> count + collection.size(), Integer::sum);
-        Collection<T> result = new ArrayList<>(size);
-        for (Collection<T> collection : collections) {
-            result.addAll(collection);
-        }
-        return result;
-    }
-    @SafeVarargs
-    static <T> T[] combine(T[] ... collections) {
-        int size = Arrays.stream(collections).reduce(0, (count, collection) -> count + collection.length, Integer::sum);
-        List<T> result = new ArrayList<>(size);
-        for (T[] collection : collections) {
-            Collections.addAll(result, collection);
-        }
-        // Can't initialize generic arrays I guess.
-        @SuppressWarnings("unchecked")
-        T[] arr = (T[])new Object[result.size()];
-        return result.toArray(arr);
-    }
 }
