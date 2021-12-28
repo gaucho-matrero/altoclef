48082a4b835a7a811f248cef5b37af6a7bc710a3
Jgreen <james.green31@protonmail.ch>
2021-12-28 00:10:23 -0700

    GearUpCommand now works as expected. Trying to optimize this so that it will place crafting tables if one is too far away. May change this to be a global thing as walking 100 miles for a crafting table we have in our hand is stupid.
    This commit is not Finished and is to save progress.
    
    Signed-off-by: Jgreen <james.green31@protonmail.ch>

--- src/main/java/adris/altoclef/tasks/misc/GatherStrongGearTask.java
+++ src/main/java/adris/altoclef/tasks/misc/GatherStrongGearTask.java
@@ -3,13 +3,19 @@ package adris.altoclef.tasks.misc;
 
 import adris.altoclef.AltoClef;
 import adris.altoclef.TaskCatalogue;
+import adris.altoclef.tasks.construction.PlaceBlockNearbyTask;
+import adris.altoclef.tasks.construction.PlaceBlockTask;
 import adris.altoclef.tasksystem.Task;
 import adris.altoclef.util.ArmorRequirement;
 import adris.altoclef.util.ItemTarget;
 import adris.altoclef.util.helpers.ItemHelper;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class GatherStrongGearTask extends Task {
 
@@ -37,24 +43,47 @@ public class GatherStrongGearTask extends Task {
 
     @Override
     protected Task onTick(AltoClef mod) {
+        setDebugState("Getting better gear");
         int TaskOrderVariable =  (mod.getInventoryTracker().hasItem(new ItemTarget(Items.DIAMOND_PICKAXE))
                 ?3:0)
                 +(Arrays.stream(ItemHelper.DIAMOND_ARMORS).allMatch(armor -> mod.getInventoryTracker().isArmorEquipped(armor))?5:0)
                 +(mod.getInventoryTracker().hasAllItems(Items.DIAMOND_HOE,
                 Items.DIAMOND_SWORD,Items.DIAMOND_SHOVEL,Items.DIAMOND_AXE)?7
                 :0); // Clever boolean check
+        ItemEntity closest = mod.getEntityTracker().getClosestItemDrop(mod.getPlayer().getPos(), Items.CRAFTING_TABLE);
+
         switch(TaskOrderVariable){
+
             case 0, 5, 7, 12 -> { // We have nothing...fuck
-                return _getPickTask;
+                setDebugState("Getting better pickaxe");
+                if(mod.getInventoryTracker().hasItem(Items.CRAFTING_TABLE)) {
+                    return _getPickTask;
+                }else{
+                    return TaskCatalogue.getSquashedItemTask(new ItemTarget(Items.CRAFTING_TABLE));
+                }
 
             }
-            case 3, 10 -> { // We have only the pick
-                return _equipArmorTask;
+            case 3, 10 -> { // We have only the pick or we have everything
+                // but armor
+                setDebugState("Getting better armor");
+
+
+                if (mod.getInventoryTracker().hasItem(Items.CRAFTING_TABLE) && !closest.isInRange(mod.getPlayer(), 10)){
+                    // TODO Place crafting table instead of going after it.
+                    return _equipArmorTask;
+                }else{
+                    return TaskCatalogue.getSquashedItemTask(new ItemTarget(Items.CRAFTING_TABLE));
+                }
 
             }
 
             case 8 -> { //We have the pick and the armor but still need tools
-                return _getToolsTask;
+                setDebugState("Getting better tools");
+                if(mod.getInventoryTracker().hasItem(Items.CRAFTING_TABLE)) {
+                    return _getToolsTask;
+                }else{
+                    return TaskCatalogue.getSquashedItemTask(new ItemTarget(Items.CRAFTING_TABLE));
+                }
             }
             default -> {
                 stop(mod);
