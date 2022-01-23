914a621f0206e41908dbec335704f2a782de8e6a
Adris Jautakas <adrisj7@gmail.com>
2022-01-23 16:06:15 -0500

    green man music

--- src/main/java/adris/altoclef/util/helpers/EntityHelper.java
+++ src/main/java/adris/altoclef/util/helpers/EntityHelper.java
@@ -3,12 +3,18 @@ package adris.altoclef.util.helpers;
 import adris.altoclef.AltoClef;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.DamageUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.mob.HoglinEntity;
 import net.minecraft.entity.mob.PiglinEntity;
 import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 
@@ -55,5 +61,46 @@ public class EntityHelper {
             }
         }
         return false;
+    }
+
+    /**
+     * Calculate the resulting damage dealt to a player as a result of some damage.
+     * If this player were to receive this damage, the player's health will be subtracted by the resulting value.
+     */
+    public static double calculateResultingPlayerDamage(PlayerEntity player, DamageSource source, double damageAmount) {
+        // Copied logic from `PlayerEntity.applyDamage`
+
+        if (player.isInvulnerableTo(source))
+            return 0;
+
+        // Armor Base
+        if (!source.bypassesArmor()) {
+            damageAmount = DamageUtil.getDamageLeft((float) damageAmount, (float) player.getArmor(), (float) player.getAttributeValue(EntityAttributes.GENERIC_ARMOR_TOUGHNESS));
+        }
+
+        // Enchantments & Potions
+        if (!source.isUnblockable()) {
+            int k;
+            if (player.hasStatusEffect(StatusEffects.RESISTANCE) && source != DamageSource.OUT_OF_WORLD) {
+                k = (player.getStatusEffect(StatusEffects.RESISTANCE).getAmplifier() + 1) * 5;
+                int j = 25 - k;
+                double f = damageAmount * (double)j;
+                double g = damageAmount;
+                damageAmount = Math.max(f / 25.0F, 0.0F);
+            }
+
+            if (damageAmount <= 0.0) {
+                damageAmount = 0.0;
+            } else {
+                k = EnchantmentHelper.getProtectionAmount(player.getArmorItems(), source);
+                if (k > 0) {
+                    damageAmount = DamageUtil.getInflictedDamage((float)damageAmount, (float)k);
+                }
+            }
+        }
+
+        // Absorption
+        damageAmount = Math.max(damageAmount - player.getAbsorptionAmount(), 0.0F);
+        return damageAmount;
     }
 }
