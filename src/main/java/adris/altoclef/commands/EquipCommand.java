5ab55040d8daf2d4e62e114e2b5c46277948ba26
Jgreen <james.green31@protonmail.ch>
2022-01-24 15:00:14 -0700

    Added javadoc and comments to code
    Signed-off-by: Jgreen <james.green31@protonmail.ch>

--- src/main/java/adris/altoclef/commands/EquipCommand.java
+++ src/main/java/adris/altoclef/commands/EquipCommand.java
@@ -8,15 +8,24 @@ import adris.altoclef.util.ItemTarget;
 import net.minecraft.item.Items;
 
 public class EquipCommand extends Command {
+    /**
+     * Constructor for this command.
+     * @throws CommandException
+     */
     public EquipCommand() throws CommandException {
         super("equip", "Equips armor", new Arg(ItemList.class,
                 "[armors]"));
     }
-
+    /**
+     * Executes or runs a task when called. Accepts parameters from [parser]
+     * @param mod Needs access to Minecraft
+     * @param parser Grabs user input from command line
+     * @throws CommandException
+     */
     @Override
     protected void call(AltoClef mod, ArgParser parser) throws CommandException {
         ItemTarget[] items = parser.get(ItemList.class).items;
-        if(items[0].matches(Items.DIAMOND)){
+        if(items[0].matches(Items.DIAMOND)){ //shortcut for full armor sets
             mod.runUserTask(new EquipArmorTask(Items.DIAMOND_CHESTPLATE, Items.DIAMOND_LEGGINGS, Items.DIAMOND_HELMET, Items.DIAMOND_BOOTS),this::finish);
         }else if(items[0].matches(Items.NETHERITE_INGOT)){
             mod.runUserTask(new EquipArmorTask(Items.NETHERITE_CHESTPLATE, Items.NETHERITE_LEGGINGS, Items.NETHERITE_HELMET, Items.NETHERITE_BOOTS),this::finish);
@@ -25,7 +34,7 @@ public class EquipCommand extends Command {
         }else if(items[0].matches(Items.LEATHER)){
             mod.runUserTask(new EquipArmorTask(Items.LEATHER_CHESTPLATE, Items.LEATHER_LEGGINGS, Items.LEATHER_HELMET, Items.LEATHER_BOOTS),this::finish);
         }else{
-            for(ItemTarget item:items){
+            for(ItemTarget item:items){ // Check for bad items
                 if(!item.matches(Items.DIAMOND_CHESTPLATE) && !item.matches(Items.DIAMOND_LEGGINGS) && !item.matches(Items.DIAMOND_HELMET) && !item.matches(Items.DIAMOND_BOOTS) &&
                         !item.matches(Items.LEATHER_CHESTPLATE) && !item.matches(Items.NETHERITE_LEGGINGS) && !item.matches(Items.NETHERITE_HELMET) && !item.matches(Items.NETHERITE_BOOTS) &&
                         !item.matches(Items.IRON_CHESTPLATE) && !item.matches(Items.IRON_LEGGINGS) && !item.matches(Items.IRON_HELMET) && !item.matches(Items.IRON_BOOTS) &&
