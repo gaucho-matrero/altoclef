4b32c14ed665b281de5e8341dbc075e47292b396
MarvionKirito <marvionkirito@gmail.com>
2023-12-21 17:28:15 +0800

    A little refactoring.

--- src/main/java/adris/altoclef/util/helpers/ConfigHelper.java
+++ src/main/java/adris/altoclef/util/helpers/ConfigHelper.java
@@ -12,11 +12,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -31,18 +29,40 @@ public class ConfigHelper {
     // For reloading
     private static final HashMap<String, Runnable> _loadedConfigs = new HashMap<>();
 
+    /**
+     * Returns a File object representing the configuration file located at the given path.
+     *
+     * @param path The relative path of the configuration file.
+     * @return The File object representing the configuration file.
+     */
     private static File getConfigFile(String path) {
-        return Paths.get(ALTO_FOLDER, path).toFile();
+        // Get the full path by concatenating the ALTO_FOLDER and the given path
+        String fullPath = ALTO_FOLDER + File.separator + path;
+
+        // Create a new File object using the full path
+        return new File(fullPath);
     }
 
+    /**
+     * Reloads all configurations.
+     */
     public static void reloadAllConfigs() {
-        if (!_loadedConfigs.values().isEmpty()) {
-            for (Runnable reload : _loadedConfigs.values()) {
-                reload.run();
-            }
+        for (Runnable config : _loadedConfigs.values()) {
+            config.run();
         }
     }
 
+    /**
+     * Retrieves the configuration from the specified path.
+     * If the configuration file does not exist, it creates a new one using the default value.
+     * If there is an error reading or parsing the configuration file, it returns the default value.
+     *
+     * @param path        The path to the configuration file.
+     * @param getDefault  A supplier that provides the default value for the configuration.
+     * @param classToLoad The class of the configuration object.
+     * @param <T>         The type of the configuration object.
+     * @return The retrieved configuration object or the default value.
+     */
     private static <T> T getConfig(String path, Supplier<T> getDefault, Class<T> classToLoad) {
         T result = getDefault.get();
         File loadFrom = getConfigFile(path);
@@ -58,7 +78,6 @@ public class ConfigHelper {
         module.addDeserializer(BlockPos.class, new BlockPosDeserializer());
         mapper.registerModule(module);
 
-        boolean failed = false;
         try {
             result = mapper.readValue(loadFrom, classToLoad);
         } catch (JsonMappingException ex) {
@@ -66,116 +85,241 @@ public class ConfigHelper {
             ex.printStackTrace();
             if (result instanceof IFailableConfigFile failable)
                 failable.failedToLoad();
-            failed = true;
+            return result;
         } catch (IOException e) {
             Debug.logError("Failed to read Config at " + path + ".");
             e.printStackTrace();
             if (result instanceof IFailableConfigFile failable)
                 failable.failedToLoad();
-            failed = true;
+            return result;
         }
 
-        // Save over to include NEW settings
-        // but only if a load was successful. Don't want to override user settings!
-        if (!failed) {
-            saveConfig(path, result);
-        }
+        saveConfig(path, result);
 
         return result;
     }
 
+    /**
+     * Load the configuration from the given path, using the provided default value,
+     * class to load, and callback function for when the configuration is reloaded.
+     *
+     * @param path        The path to the configuration file.
+     * @param getDefault  A supplier function that provides the default value of the configuration.
+     * @param classToLoad The class of the configuration object to load.
+     * @param onReload    A consumer function that is called when the configuration is reloaded.
+     * @param <T>         The type of the configuration object.
+     */
     public static <T> void loadConfig(String path, Supplier<T> getDefault, Class<T> classToLoad, Consumer<T> onReload) {
-        T result = getConfig(path, getDefault, classToLoad);
-        _loadedConfigs.put(path, () -> onReload.accept(getConfig(path, getDefault, classToLoad)));
-        onReload.accept(result);
+        // Get the configuration object using the getConfig function.
+        T config = getConfig(path, getDefault, classToLoad);
+
+        // Store the configuration object and the reload callback in the loadedConfigs map.
+        _loadedConfigs.put(path, () -> onReload.accept(config));
+
+        // Call the onReload callback function to notify that the configuration is loaded.
+        onReload.accept(config);
     }
 
+    /**
+     * Save the configuration object to a file at the specified path.
+     *
+     * @param path   The path of the file to save the configuration to.
+     * @param config The configuration object to be saved.
+     */
     public static <T> void saveConfig(String path, T config) {
+        // Create an object mapper to serialize the configuration object
         ObjectMapper mapper = new ObjectMapper();
+
+        // Create a module to register custom serializers for specific classes
         SimpleModule module = new SimpleModule();
         module.addSerializer(Vec3d.class, new Vec3dSerializer());
         module.addSerializer(BlockPos.class, new BlockPosSerializer());
         module.addSerializer(ChunkPos.class, new ChunkPosSerializer());
         mapper.registerModule(module);
 
-        File toSave = getConfigFile(path);
-        if (!toSave.getParentFile().exists()) {
-            //noinspection ResultOfMethodCallIgnored
-            toSave.getParentFile().mkdirs();
-        }
+        // Get the file object for the specified path
+        File configFile = getConfigFile(path);
+
+        // Create parent directories if they don't exist
+        createParentDirectories(configFile);
 
         try {
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            // Enable pretty printing for the serialized JSON
+            enablePrettyPrinting(mapper);
 
-            // Pretty print and indent arrays too.
-            DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
-            prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
-
-            mapper.writer(prettyPrinter).writeValue(toSave, config);
+            // Write the serialized configuration object to the file
+            writeConfigToFile(mapper, configFile, config);
         } catch (IOException e) {
-            e.printStackTrace();
+            // Handle any IO exceptions that occur during the write process
+            handleIOException(e);
         }
     }
 
+    /**
+     * Creates the parent directories for a given file.
+     *
+     * @param file the file to create parent directories for
+     */
+    private static void createParentDirectories(File file) {
+        try {
+            // Get the parent path of the file
+            Path parentPath = file.getParentFile().toPath();
+
+            // Create the parent directories
+            Files.createDirectories(parentPath);
+        } catch (IOException e) {
+            // Print an error message if failed to create parent directories
+            System.err.println("Failed to create parent directories: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Enable pretty printing for the given ObjectMapper.
+     *
+     * @param mapper The ObjectMapper to enable pretty printing for.
+     */
+    private static void enablePrettyPrinting(ObjectMapper mapper) {
+        // Check if the ObjectMapper is not null
+        if (mapper != null) {
+            // Enable indentation for the output
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            // Create a DefaultPrettyPrinter with a line feed indenter
+            DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+            prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+
+            // Set the pretty printer for the ObjectMapper's writer
+            mapper.writer(prettyPrinter);
+        }
+    }
+
+    /**
+     * Writes the given configuration data to the specified file using the provided ObjectMapper.
+     *
+     * @param objectMapper the ObjectMapper used to serialize the configuration data
+     * @param configFile   the file to write the configuration data to
+     * @param configData   the configuration data to write to the file
+     * @throws IOException if an I/O error occurs while writing to the file
+     */
+    private static <T> void writeConfigToFile(ObjectMapper objectMapper, File configFile, T configData) throws IOException {
+        try (Writer writer = new FileWriter(configFile)) {
+            objectMapper.writeValue(writer, configData);
+        }
+    }
+
+    /**
+     * Handles an IOException by printing an error message to the standard error stream.
+     *
+     * @param exception The IOException to handle.
+     */
+    private static void handleIOException(IOException exception) {
+        // Create an error message with the exception message
+        String errorMessage = "An IOException occurred: " + exception.getMessage();
+
+        // Print the error message to the standard error stream
+        System.err.println(errorMessage);
+    }
+
+    /**
+     * Retrieves a list configuration from the specified path.
+     *
+     * @param path       The path of the configuration file.
+     * @param getDefault A supplier that provides a default configuration object.
+     * @param <T>        The type of the configuration object.
+     * @return The retrieved configuration object, or null if an error occurs.
+     */
     private static <T extends IListConfigFile> T getListConfig(String path, Supplier<T> getDefault) {
         T result = getDefault.get();
-
         result.onLoadStart();
 
-        File loadFrom = getConfigFile(path);
-        if (!loadFrom.exists()) {
-            // Empty
-            Debug.logInternal("Lists file not found at " + path);
+        File configFile = getConfigFile(path);
+        if (!configFile.exists()) {
             return result;
         }
 
-        try {
-            FileInputStream fis = new FileInputStream(loadFrom);
-            Scanner sc = new Scanner(fis);    //file to be scanned
-            //returns true if there is another line to read
-            while (sc.hasNextLine()) {
-                String line = trimComment(sc.nextLine()).trim();
-                if (line.length() == 0) continue;
+        try (FileInputStream fis = new FileInputStream(configFile);
+             Scanner scanner = new Scanner(fis)) {
+            while (scanner.hasNextLine()) {
+                String line = trimComment(scanner.nextLine()).trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
                 result.addLine(line);
             }
-            sc.close();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
+
         return result;
     }
 
+    /**
+     * Loads a list configuration file from the given path.
+     *
+     * @param path       the path of the configuration file
+     * @param getDefault a supplier function that provides a default configuration object
+     * @param onReload   a consumer function that handles the reload of the configuration object
+     * @param <T>        the type of the configuration object
+     */
     public static <T extends IListConfigFile> void loadListConfig(String path, Supplier<T> getDefault, Consumer<T> onReload) {
+        // Get the configuration object from the specified path
         T result = getListConfig(path, getDefault);
-        _loadedConfigs.put(path, () -> onReload.accept(getListConfig(path, getDefault)));
+
+        // Store a lambda function in the map to handle the reload of the configuration object
+        _loadedConfigs.put(path, () -> onReload.accept(result));
+
+        // Trigger the reload of the configuration object
         onReload.accept(result);
     }
 
+    /**
+     * This method trims a comment from the given line.
+     * If the line does not contain a comment, the original line is returned.
+     *
+     * @param line The line to trim the comment from
+     * @return The line with the comment trimmed
+     */
     private static String trimComment(String line) {
-        int pound = line.indexOf('#');
-        if (pound == -1) {
+        int poundIndex = line.indexOf('#');
+        if (poundIndex == -1) {
             return line;
+        } else {
+            return line.substring(0, poundIndex);
         }
-        return line.substring(0, pound);
     }
 
+    /**
+     * Ensures that the commented list file exists at the specified path.
+     *
+     * @param path            The path where the commented list file should be located.
+     * @param startingComment The starting comment for the file.
+     */
     public static void ensureCommentedListFileExists(String path, String startingComment) {
-        File loadFrom = getConfigFile(path);
-        if (loadFrom.exists()) {
-            // Already exists, don't make a new one.
+        File configFile = getConfigFile(path);
+        if (configFile.exists()) {
             return;
         }
-        StringBuilder result = new StringBuilder();
+        StringBuilder commentBuilder = new StringBuilder();
         for (String line : startingComment.split("\\r?\\n")) {
-            if (line.length() != 0) {
-                result.append("# ").append(line).append("\n");
+            if (!line.isEmpty()) {
+                commentBuilder.append("# ").append(line).append("\n");
             }
         }
         try {
-            Files.write(loadFrom.toPath(), result.toString().getBytes());
+            Files.write(configFile.toPath(), commentBuilder.toString().getBytes());
         } catch (IOException e) {
-            e.printStackTrace();
+            handleException(e);
         }
+    }
+
+    /**
+     * Handles an IOException by printing an error message to the standard error stream.
+     *
+     * @param exception The IOException to handle.
+     */
+    private static void handleException(IOException exception) {
+        // Print the error message to the standard error stream
+        System.err.println("An error occurred: " + exception.getMessage());
     }
 }
